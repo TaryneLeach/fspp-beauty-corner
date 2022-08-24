366b3cdebb1cd51d6b9fb0724f2af204fdbd8901
TaryneLeach <taryneleach@gmail.com>
2022-08-23 22:10:57 -0400

    added Trello link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# fspp-beauty-corner
+Welcome To The Beauty Corner!
+
+Trello [https://trello.com/b/71AmpUV3/the-beauty-corner]
---
+++ back-end/app.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+const cors = require('cors');
+const express = require('express');
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// CONFIGURATION
+const app = express();
+
+// ROUTES
+
+app.get('/', (req, res) => {
+	res.send('Welcome To The Girls Room!');
+});
+
+app.get('*', (req, res) => {
+	res.status(404).send('Took a wrong turn!');
+});
